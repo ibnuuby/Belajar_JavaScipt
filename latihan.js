3bc4a7f9068d70441fcde15cd9f8fe99a5dabb72
Ibnu Ubaeydillah <ibnuubaeydillah13@gmail.com>
2021-08-15 08:05:46 +0700

    push 1

---
+++ latihan.js
@@ -0,0 +1,30 @@
+/**
+ * TODO:
+ * Buatlah variabel firstName, lastName, age, isMarried dengan ketentuan:
+ *  - firstName: bertipe data string, dengan nilai nama depan Anda.
+ *  - lastName: bertipe data string, dengan nilai nama belakang Anda.
+ *  - age: bertipe data number, dengan nilai umur Anda.
+ *  - isMarried: bertipe data boolean, dengan bebas Anda tentukan.
+ */
+
+// TODO
+
+/**
+ * Hiraukan kode di bawah ini
+ */
+
+// module.exports = {
+//   firstName, lastName, age, isMarried,
+// };
+
+let firstName 
+let lastName  
+let age      
+let isMarried 
+
+firstName = "Ibnu";
+lastName  = "Ubaeydillah";
+age = 22;
+isMarried = "Single";
+
+console.log(firstName,lastName,age,isMarried);
