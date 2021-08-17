722c3a6adeea2eb655c29b34240610974a96693a
ibnuubaeydillah13 <ibnuubaeydillah13@gmail.com>
2021-08-17 11:26:31 +0700

    struktur data

---
+++ Struktur Data/object.js
@@ -0,0 +1,9 @@
+let fruit = {
+    name : 'grape',
+    description : 'Popular and famous fruit.',
+    colors : ['purple','green'],
+    isSmall: true,
+    order : 1
+}
+
+console.log(fruit["order"]);
