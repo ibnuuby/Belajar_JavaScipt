0d26897c865497cee0d5e9d549f3f6735a4ab403
ibnuubaeydillah13 <ibnuubaeydillah13@gmail.com>
2021-08-22 22:34:12 +0700

    array max

---
+++ array-max.js
@@ -0,0 +1,6 @@
+// Find big Array............!
+var auntorSalary = 250;
+var salajSalary = 700;
+var amdadulSalary = 950;
+var findMax = Math.max(auntorSalary, salajSalary ,amdadulSalary);
+console.log(findMax);
